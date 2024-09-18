34a492875d7fa729d8c31d5087b9d12bc954fdfa
Desiree Wright <xv7406ml@go.minneapolis.edu>
2024-09-18 22:34:42 +0000

    initialcomit

--- Module3Lab/obj/Debug/net8.0/Module3Lab.AssemblyInfo.cs
+++ Module3Lab/obj/Debug/net8.0/Module3Lab.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Module3Lab")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38cda2679d74c2e7670bf57daed9b2a2c18dd863")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ca43a91d17886a826cdc18339b45fb69d3ff622")]
 [assembly: System.Reflection.AssemblyProductAttribute("Module3Lab")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Module3Lab")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
